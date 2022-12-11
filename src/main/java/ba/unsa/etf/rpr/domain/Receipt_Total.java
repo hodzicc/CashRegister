040fdfbfc88158bc6deca77e4295dcbf951e4a11
hodzicc <ahodzic1@etf.unsa.ba>
2022-12-11 02:40:55 +0100

    implementation of setters and getters in Receipt_Total

--- src/main/java/ba/unsa/etf/rpr/domain/Receipt_Total.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Receipt_Total.java
@@ -9,5 +9,35 @@ public class Receipt_Total {
     Date date;
     int Eid;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getEid() {
+        return Eid;
+    }
+
+    public void setEid(int eid) {
+        Eid = eid;
+    }
 }
