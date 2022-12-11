62e2604d2d79508c20208fc255fac5d4f3d18663
hodzicc <ahodzic1@etf.unsa.ba>
2022-12-11 02:54:58 +0100

    implementation of add method in Receipt_TotalDAOSQLImpl

--- src/main/java/ba/unsa/etf/rpr/DAO/Receipt_TotalDAOSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/DAO/Receipt_TotalDAOSQLImpl.java
@@ -2,8 +2,7 @@ package ba.unsa.etf.rpr.DAO;
 
 import ba.unsa.etf.rpr.domain.Receipt_Total;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 import java.util.List;
 
 public class Receipt_TotalDAOSQLImpl implements  Receipt_TotalDAO{
@@ -19,6 +18,29 @@ public class Receipt_TotalDAOSQLImpl implements  Receipt_TotalDAO{
     }
     @Override
     public Receipt_Total add(Receipt_Total item) {
+        String insert = "INSERT INTO Receipt_total(ID_receipt, Total, Date, Employee_ID) VALUES(?)";
+
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+
+
+            stmt.setInt(1, item.getId());
+            stmt.setDouble(2,item.getTotal());
+            stmt.setDate(3, (Date) item.getDate());
+            stmt.setInt(4,item.getEid());
+
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+
+            item.setId(rs.getInt(1));
+            return item;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
         return null;
     }
 
