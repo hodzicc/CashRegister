840dbba5a1b5924f9f20f8230ff73cb8d4e10393
hodzicc <ahodzic1@etf.unsa.ba>
2022-12-11 02:06:10 +0100

    implementation of setters and getters in Receipts

--- src/main/java/ba/unsa/etf/rpr/domain/Receipts.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Receipts.java
@@ -6,4 +6,36 @@ public class Receipts {
     private int idP;
     private int Quantity;
     private double LineTotal;
+
+    public int getIdR() {
+        return idR;
+    }
+
+    public void setIdR(int idR) {
+        this.idR = idR;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+
+    public int getIdP() {
+        return idP;
+    }
+
+    public void setIdP(int idP) {
+        this.idP = idP;
+    }
+
+    public double getLineTotal() {
+        return LineTotal;
+    }
+
+    public void setLineTotal(double lineTotal) {
+        LineTotal = lineTotal;
+    }
 }
