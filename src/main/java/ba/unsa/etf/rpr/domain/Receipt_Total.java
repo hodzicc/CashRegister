1cd563ea4d56bb73bc77169f6502ad4d7c2eb65f
hodzicc <ahodzic1@etf.unsa.ba>
2022-12-11 02:42:17 +0100

    implementation of equals and hashCode in Receipt_Total

--- src/main/java/ba/unsa/etf/rpr/domain/Receipt_Total.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Receipt_Total.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr.domain;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Receipt_Total {
 
@@ -39,5 +40,18 @@ public class Receipt_Total {
 
     public void setEid(int eid) {
         Eid = eid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Receipt_Total)) return false;
+        Receipt_Total that = (Receipt_Total) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id,total,date,Eid);
     }
 }
