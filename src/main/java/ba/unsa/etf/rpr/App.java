2f11f6855726ebcfb64ca1ba5fc2fd1f6db0ef57
hodzicc <ahodzic1@etf.unsa.ba>
2022-12-19 02:36:47 +0100

    adding javafx plugin

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,13 +1,22 @@
 package ba.unsa.etf.rpr;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App extends Application {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
