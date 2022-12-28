9b427d90c1199b9ea846e328a0be4c0c34d9099a
hodzicc <ahodzic1@etf.unsa.ba>
2022-12-28 22:14:01 +0100

    changes in controller for user registration

--- src/main/java/ba/unsa/etf/rpr/controllers/UserRegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserRegistrationController.java
@@ -1,5 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+
 public class UserRegistrationController {
 
+
+    public TextField UsernameField;
+    public javafx.scene.control.PasswordField PasswordField;
+    public TextField NameField;
+    public Button CancelBtn;
+    public Button SaveBtn;
 }
