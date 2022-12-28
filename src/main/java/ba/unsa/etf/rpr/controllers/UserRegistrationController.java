84c07ccfe0937003add4fff978514d5690f17e2d
hodzicc <ahodzic1@etf.unsa.ba>
2022-12-28 23:34:19 +0100

    adding initializer method with listeners

--- src/main/java/ba/unsa/etf/rpr/controllers/UserRegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserRegistrationController.java
@@ -1,17 +1,71 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.DAO.EmployeesDAOSQLImpl;
+import ba.unsa.etf.rpr.domain.Employees;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserRegistrationController {
-
 
     public TextField UsernameField;
     public javafx.scene.control.PasswordField PasswordField;
     public TextField NameField;
     public Button CancelBtn;
     public Button SaveBtn;
+    public Label FullNameCheck;
+    public Label UsernameCheck;
+    public Label PasswordCheck;
+
+    public void initialize(){
+
+        EmployeesDAOSQLImpl sqlimpl = new EmployeesDAOSQLImpl();
+        List<Employees> empl = new ArrayList<>();
+        empl = sqlimpl.getAll();
+
+        List<Employees> finalEmpl = empl;
+        NameField.textProperty().addListener((obs, oldValue, newValue)->{
+
+            for(Employees e: finalEmpl) {
+
+                if (e.getName().equals(newValue)) {
+                    FullNameCheck.setText("That user is already registered");
+                   break;
+                }
+               else    FullNameCheck.setText("");
+            }
+        });
+        UsernameField.textProperty().addListener((obs, oldValue, newValue)->{
+
+            for(Employees e: finalEmpl) {
+
+                if (e.getUsername().equals(newValue)) {
+                    UsernameCheck.setText("That username already exists");
+                    break;
+                }
+                else    UsernameCheck.setText("");
+            }
+        });
+        PasswordField.textProperty().addListener((obs, oldValue, newValue)->{
+
+                if (newValue.length()<=4) {
+                    PasswordCheck.setText("Password too short");
+
+                }
+                else    PasswordCheck.setText("");
+
+        });
+
+
+
+
+    }
+
+
+
+
+
 }
