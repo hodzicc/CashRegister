43e5f31c60b22df042173c648d74387e7cf6137c
hodzicc <ahodzic1@etf.unsa.ba>
2022-12-29 23:27:29 +0100

    importing products from db to tableview

--- src/main/java/ba/unsa/etf/rpr/controllers/ProductsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ProductsController.java
@@ -1,8 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.DAO.ProductsDAOSQLImpl;
+import ba.unsa.etf.rpr.domain.Products;
+import javafx.collections.FXCollections;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductsController {
     public Button addBtn;
@@ -10,12 +17,24 @@ public class ProductsController {
     public Button DeleteBtn;
     public Button ExitBtn;
     public TableView productsTable;
-    public TableColumn ProductIdCol;
-    public TableColumn ProductNameCol;
-    public TableColumn ProductPriceCol;
-    public TableColumn LeftInStockCol;
+    public TableColumn<Products,Integer> ProductIdCol;
+    public TableColumn<Products, String> ProductNameCol;
+    public TableColumn<Products, Double> ProductPriceCol;
+    public TableColumn<Products, Integer> LeftInStockCol;
 
     public void initialize(){
+
+
+        ProductIdCol.setCellValueFactory(new PropertyValueFactory<Products, Integer>("id"));
+        ProductNameCol.setCellValueFactory(new PropertyValueFactory<Products, String>("name"));
+        ProductPriceCol.setCellValueFactory(new PropertyValueFactory<Products, Double>("price"));
+        LeftInStockCol.setCellValueFactory(new PropertyValueFactory<Products, Integer>("leftInStock"));
+
+        ProductsDAOSQLImpl products = new ProductsDAOSQLImpl();
+
+        productsTable.setItems(FXCollections.observableList(products.getAll()));
+        productsTable.refresh();
+
 
     }
 }
