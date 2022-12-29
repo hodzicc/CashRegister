9f83febfa8556c7c16f3916a750701805d49f352
hodzicc <ahodzic1@etf.unsa.ba>
2022-12-30 00:20:03 +0100

    declaring buttons and textfields in controller

--- src/main/java/ba/unsa/etf/rpr/controllers/AddProductController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AddProductController.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class AddProductController {
+    public Button saveBtn;
+    public Button cancelBtn;
+    public TextField nameField;
+    public TextField priceField;
+    public TextField leftField;
+    public Label nameCheck;
 }
