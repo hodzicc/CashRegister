ab7b2d8bed9af45786fe99aec45f386aca25ff98
hodzicc <ahodzic1@etf.unsa.ba>
2022-12-29 21:33:55 +0100

    creating fields for buttons and tableview

--- src/main/java/ba/unsa/etf/rpr/controllers/ProductsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ProductsController.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
 public class ProductsController {
+    public Button addBtn;
+    public Button UpdateBtn;
+    public Button DeleteBtn;
+    public Button ExitBtn;
+    public TableView productsTable;
 }
