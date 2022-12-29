d26c0f4d066d642bda463414ad49e03392a81120
hodzicc <ahodzic1@etf.unsa.ba>
2022-12-29 21:55:07 +0100

    adding columns to tableview

--- src/main/java/ba/unsa/etf/rpr/controllers/ProductsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ProductsController.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class ProductsController {
@@ -9,4 +10,12 @@ public class ProductsController {
     public Button DeleteBtn;
     public Button ExitBtn;
     public TableView productsTable;
+    public TableColumn ProductIdCol;
+    public TableColumn ProductNameCol;
+    public TableColumn ProductPriceCol;
+    public TableColumn LeftInStockCol;
+
+    public void initialize(){
+
+    }
 }
