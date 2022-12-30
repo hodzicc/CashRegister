064230d7301b318ede1c2bf075b1e58da1b56b30
hodzicc <ahodzic1@etf.unsa.ba>
2022-12-30 22:18:34 +0100

    creating fields in controller

--- src/main/java/ba/unsa/etf/rpr/controllers/UpdateController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UpdateController.java
@@ -1,5 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class UpdateController {
 
+    public Label idLabel;
+    public Label nameLabel;
+    public TextField priceField;
+    public TextField lisField;
+    public Button saveBtn;
+    public Button cancelBtn;
 }
