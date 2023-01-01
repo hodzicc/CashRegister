0cb9dd744deca8bfb12eb8c58c4d2bfc258f9f36
hodzicc <ahodzic1@etf.unsa.ba>
2023-01-01 21:56:20 +0100

    creating fields in controller

--- src/main/java/ba/unsa/etf/rpr/controllers/MakeReceiptController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MakeReceiptController.java
@@ -1,4 +1,20 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
 public class MakeReceiptController {
+
+    public Button exitBtn;
+    public Button makeBtn;
+    public Button addBtn;
+    public Label checkQuantityLabel;
+    public TextField quantityField;
+    public Label checkIdLabel;
+    public TextField idField;
+    public VBox quantityCol;
+    public TableColumn totalCol;
+    public TableColumn unitPriceCol;
+    public TableColumn nameCol;
+    public TableView productsTable;
 }
