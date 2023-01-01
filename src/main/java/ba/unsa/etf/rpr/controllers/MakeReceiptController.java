f44cc3a064148e828ad59894177a04e3a89efea0
hodzicc <ahodzic1@etf.unsa.ba>
2023-01-01 22:24:10 +0100

    added listener for idField

--- src/main/java/ba/unsa/etf/rpr/controllers/MakeReceiptController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MakeReceiptController.java
@@ -1,11 +1,18 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.DAO.ProductsDAOSQLImpl;
+import ba.unsa.etf.rpr.domain.Products;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class MakeReceiptController {
 
+    private int id;
     public Button exitBtn;
     public Button makeBtn;
     public Button addBtn;
@@ -19,6 +26,33 @@ public class MakeReceiptController {
     public TableColumn nameCol;
     public TableView productsTable;
     public void initialize(){
+
+
+        ProductsDAOSQLImpl sqlimpl = new ProductsDAOSQLImpl();
+        List<Products> prod = new ArrayList<>();
+        prod = sqlimpl.getAll();
+
+        List<Products> finalProd= prod;
+
+        idField.textProperty().addListener((obs, oldValue, newValue)->{
+        int ok = 0;
+
+            for(Products p: finalProd) {
+
+                if (p.getId()==Integer.parseInt(newValue)) {
+                    ok=1;
+                   break;
+                }
+
+            }
+            if(ok==1)
+            checkIdLabel.setText("");
+            else checkIdLabel.setText("There is no product with that id");
+
+        });
+
+
+
 
 
     }
