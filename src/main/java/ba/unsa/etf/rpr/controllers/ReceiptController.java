9731b8a8f3dab8d9835c861381ea940bf15b8f91
hodzicc <ahodzic1@etf.unsa.ba>
2023-01-02 03:10:41 +0100

    added fields in controller

--- src/main/java/ba/unsa/etf/rpr/controllers/ReceiptController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ReceiptController.java
@@ -1,4 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ReceiptController {
+    public Button closeBtn;
+    public Label dateLabel;
+    public Label totalLabel;
+    public TableView productsTable;
+    public TableColumn nameCol;
+    public TableColumn priceCol;
+    public TableColumn quantityCol;
+    public TableColumn totalCol;
 }
