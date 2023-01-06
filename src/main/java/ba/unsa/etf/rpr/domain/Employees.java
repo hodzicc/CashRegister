2cebddd84c0f86d3a4ad0532da16228a97038e46
hodzicc <ahodzic1@etf.unsa.ba>
2023-01-07 00:55:39 +0100

    small changes because of refactoring

--- src/main/java/ba/unsa/etf/rpr/domain/Employees.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Employees.java
@@ -2,7 +2,7 @@ package ba.unsa.etf.rpr.domain;
 
 import java.util.Objects;
 
-public class Employees {
+public class Employees implements Idable{
 
     private int id;
     private String username;
