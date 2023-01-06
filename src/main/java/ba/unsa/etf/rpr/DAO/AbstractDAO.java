883d2c05f1731aa7a4f7e7cbb73e5556ea0287cb
hodzicc <ahodzic1@etf.unsa.ba>
2023-01-07 00:45:45 +0100

    implementation of update method for refactoring of dao

--- src/main/java/ba/unsa/etf/rpr/DAO/AbstractDAO.java
+++ src/main/java/ba/unsa/etf/rpr/DAO/AbstractDAO.java
@@ -158,6 +158,32 @@ public abstract class AbstractDAO<T extends Idable> implements DAO<T>{
         }
     }
 
+    public T update(T item) throws CashRegisterException{
+        Map<String, Object> row = object2row(item);
+        String updateColumns = prepareUpdateParts(row);
+        StringBuilder builder = new StringBuilder();
+        builder.append("UPDATE ")
+                .append(tableName)
+                .append(" SET ")
+                .append(updateColumns)
+                .append(" WHERE id = ?");
+
+        try{
+            PreparedStatement stmt = getConnection().prepareStatement(builder.toString());
+            int counter = 1;
+            for (Map.Entry<String, Object> entry: row.entrySet()) {
+                if (entry.getKey().equals("id")) continue; // skip ID
+                stmt.setObject(counter, entry.getValue());
+                counter++;
+            }
+            stmt.setObject(counter, item.getId());
+            stmt.executeUpdate();
+            return item;
+        }catch (SQLException e){
+            throw new CashRegisterException(e.getMessage(), e);
+        }
+    }
+
 
 
 
