48974294fb115310962381f6098bc58fb5426c22
hodzicc <ahodzic1@etf.unsa.ba>
2023-01-07 00:14:42 +0100

    implementation of createConnection method

--- src/main/java/ba/unsa/etf/rpr/DAO/AbstractDAO.java
+++ src/main/java/ba/unsa/etf/rpr/DAO/AbstractDAO.java
@@ -8,4 +8,20 @@ public abstract class AbstractDAO<T extends Idable> implements DAO<T>{
     private static Connection connection = null;
     private String tableName;
 
+    private static void createConnection(){
+        if(AbstractDAO.connection==null) {
+            try {
+                Properties p = new Properties();
+                p.load(ClassLoader.getSystemResource("application.properties").openStream());
+                String url = p.getProperty("db.connection_string");
+                String username = p.getProperty("db.username");
+                String password = p.getProperty("db.password");
+                AbstractDAO.connection = DriverManager.getConnection(url, username, password);
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }
+    }
+
 }
