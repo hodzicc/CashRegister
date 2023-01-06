497438edd799ef518262ccddf06503b6994e352d
hodzicc <ahodzic1@etf.unsa.ba>
2023-01-07 00:24:56 +0100

    implementation of constructors in CashRegisterException class

--- src/main/java/ba/unsa/etf/rpr/exceptions/CashRegisterException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/CashRegisterException.java
@@ -1,5 +1,12 @@
 package ba.unsa.etf.rpr.exceptions;
 
-public class CashRegisterException {
+public class CashRegisterException extends Exception{
+    public CashRegisterException(String msg, Exception reason){
+        super(msg, reason);
+    }
+
+    public CashRegisterException(String msg){
+        super(msg);
+    }
 
 }
