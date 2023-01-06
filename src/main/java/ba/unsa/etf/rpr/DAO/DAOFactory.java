b0de2cbc29c47d9817ea94471a08474f7b232c1b
hodzicc <ahodzic1@etf.unsa.ba>
2023-01-07 00:50:01 +0100

    adding factory method for singleton implementation of DAOs

---
+++ src/main/java/ba/unsa/etf/rpr/DAO/DAOFactory.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.DAO;
+
+public class DAOFactory {
+    /*
+    private static final EmployeesDAO employeesDAO = EmployeesDAOSQLImpl.getInstance();
+    private static final ProductsDAO productsDAO = ProductsDAOSQLImpl.getInstance();
+*/
+
+}
