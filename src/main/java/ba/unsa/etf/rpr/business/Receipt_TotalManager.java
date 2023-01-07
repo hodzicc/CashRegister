2a2f83b650f1ff4f2f0184e74bfdffec74d8f44b
hodzicc <ahodzic1@etf.unsa.ba>
2023-01-07 16:30:47 +0100

    implementation of Receipt_TotalManager in business

---
+++ src/main/java/ba/unsa/etf/rpr/business/Receipt_TotalManager.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.DAO.DAOFactory;
+import ba.unsa.etf.rpr.domain.Receipt_Total;
+import ba.unsa.etf.rpr.exceptions.CashRegisterException;
+
+import java.util.List;
+
+public class Receipt_TotalManager {
+
+
+    public List<Receipt_Total> getAll() throws CashRegisterException {
+        return DAOFactory.receipt_totalDAO().getAll();
+    }
+
+    public void delete(int id) throws CashRegisterException{
+        DAOFactory.receipt_totalDAO().delete(id);
+    }
+
+    public Receipt_Total getById(int id) throws CashRegisterException{
+        return DAOFactory.receipt_totalDAO().getById(id);
+    }
+
+    public void update(Receipt_Total r) throws CashRegisterException{
+        DAOFactory.receipt_totalDAO().update(r);
+    }
+
+    public Receipt_Total add(Receipt_Total r) throws CashRegisterException {
+        return DAOFactory.receipt_totalDAO().add(r);
+    }
+
+
+}
