16ce224feb76492ec6de7d88b9369e200b1cbdd5
hodzicc <ahodzic1@etf.unsa.ba>
2023-01-07 02:15:09 +0100

    implementation of object2row and row2object in ReceiptsDAOSQLImpl

--- src/main/java/ba/unsa/etf/rpr/DAO/ReceiptsDAOSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/DAO/ReceiptsDAOSQLImpl.java
@@ -6,10 +6,7 @@ import ba.unsa.etf.rpr.domain.Receipts;
 import ba.unsa.etf.rpr.exceptions.CashRegisterException;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 public class ReceiptsDAOSQLImpl  extends AbstractDAO<Receipts> implements ReceiptsDAO {
     private static ReceiptsDAOSQLImpl instance = null;
@@ -70,12 +67,34 @@ public class ReceiptsDAOSQLImpl  extends AbstractDAO<Receipts> implements Receip
 
     @Override
     public Receipts row2object(ResultSet rs) throws CashRegisterException {
-        return null;
+        try {
+            Receipts receipt = new Receipts();
+
+            receipt.setIdR(rs.getInt("idReceipts"));
+            receipt.setIdP(rs.getInt("idProduct"));
+            receipt.setName(rs.getString("Name"));
+            receipt.setUnitPrice(rs.getDouble("UnitPrice"));
+            receipt.setQuantity(rs.getInt("Quantity"));
+            receipt.setLineTotal(rs.getDouble("LineTotal"));
+
+            return receipt;
+
+        } catch (SQLException e) {
+            throw new CashRegisterException(e.getMessage(), e);
+        }
+
     }
 
     @Override
     public Map<String, Object> object2row(Receipts object) {
-        return null;
+        Map<String, Object> row = new TreeMap<>();
+        row.put("idReceipts", object.getIdR());
+        row.put("idProduct", object.getIdP());
+        row.put("Name", object.getName());
+        row.put("UnitPrice", object.getUnitPrice());
+        row.put("Quantity", object.getQuantity());
+        row.put("LineTotal", object.getLineTotal());
+        return row;
     }
 
     @Override
